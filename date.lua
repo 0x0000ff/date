0f99ca02c825cd25c75fd27cc0988e2e98cac9e1
0x0000ff <stamp497@googlemail.com>
2026-04-07 21:33:27 +0100

    Change square brackets to pointy brackets

--- date.lua
+++ date.lua
@@ -3,12 +3,18 @@ local buffer = import("micro/buffer")
 
 function insertDate(bp)
 	local msgStart = buffer.Loc(bp.Buf:GetActiveCursor().Loc.X, bp.Buf:GetActiveCursor().Loc.y)
-	bp.Buf:Insert(msgStart, os.date("[%d/%m/%y %a]"))
+	bp.Buf:Insert(msgStart, os.date("<%d/%m/%y>"))
+	return false
+end
+
+function insertDateWithDay(bp)
+	local msgStart = buffer.Loc(bp.Buf:GetActiveCursor().Loc.X, bp.Buf:GetActiveCursor().Loc.y)
+	bp.Buf:Insert(msgStart, os.date("<%d/%m/%y %a>"))
 	return false
 end
 
 function insertTime(bp)
 	local msgStart = buffer.Loc(bp.Buf:GetActiveCursor().Loc.X, bp.Buf:GetActiveCursor().Loc.y)
-	bp.Buf:Insert(msgStart, os.date("[%H:%M]"));
+	bp.Buf:Insert(msgStart, os.date("<%H:%M>"));
 	return false
 end
